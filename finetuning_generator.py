c031c5f2ee25632b8d8f319b3eddb571f029074f
Long Tran <longdtranw@gmail.com>
2025-03-01 21:51:27 -0700

    upda te

--- finetuning_generator.py
+++ finetuning_generator.py
@@ -16,43 +16,51 @@ with open('testing_data.json') as f:
     cases = json.load(f)
 
 module = Answerer(debug=False)
-# print("Generate finetuning data for extractor...")
-# data = []
-# for i, c in enumerate(cases):
-#     query=c["query"]
-#     articles=c["paragraphs"]
-#     expected=c["label"]
+print("Generate finetuning data for extractor...")
+data = []
+data_count = 0
+for i, c in enumerate(cases):
+    query=c["query"]
+    articles=c["paragraphs"]
+    expected=c["label"]
 
-#     failed_answer = ""
-#     count = 20
-#     while True:
-#         if count < 0:
-#             print("ignore this case, failed to many times")
-#             break
-#         count -= 1
-#         try:
-#             chat, extracted, l= module.retry_extractor(article=articles, query=query, failed_answer=failed_answer)
-#             l = "Y" if l else "N"
-#             if l == expected:
-#                 data.append({"chat": chat, "extracted": extracted})
-#                 print("Generated for case", i)
-#                 break
-#             else:
-#                 print("retrying... wrong answer")
-#                 failed_answer = extracted
-#                 continue
-#         except ExtractorException as e:
-#             print("retrying...  failed")
-#             failed_answer = e.args[0]
-#             continue
-#         except:
-#             raise
-# with open("finetuning_extractor_data.json", "w") as f:
-#     f.writelines(json.dumps(data))
-# print("Done extractor...")
+    failed_answer = ""
+    count = 20
+    while True:
+        if count < 0:
+            print("ignore this case, failed to many times")
+            break
+        count -= 1
+        try:
+            chat, extracted, l= module.retry_extractor(article=articles, query=query, failed_answer=failed_answer)
+            l = "Y" if l else "N"
+            if l == expected:
+                data.append({"chat": chat, "extracted": extracted})
+                print("Generated for case", i)
+                break
+            else:
+                print("retrying... wrong answer")
+                failed_answer = extracted
+                continue
+        except ExtractorException as e:
+            print("retrying...  failed")
+            failed_answer = e.args[0]
+            continue
+        except:
+            raise
+    if len(data) == 10: 
+        with open(f"finetuning_extractor_data_{data_count}.json", "w") as f:
+            f.writelines(json.dumps(data))
+        data_count+= 1
+        data = []
+if len(data) > 0:
+    with open(f"finetuning_extractor_data_{data_count}.json", "w") as f:
+        f.writelines(json.dumps(data))
+print("Done extractor...")
 
 print("Generate finetuning data for chat...")
 data = []
+data_count = 0
 for i, c in enumerate(cases):
     query=c["query"]
     articles=c["paragraphs"]
@@ -64,7 +72,7 @@ for i, c in enumerate(cases):
             chat, l= module.retry_chat(article=articles, query=query, failed_answer=failed_answer)
             l = "Y" if l else "N"
             if l == expected:
-                data.append({"query": query, "chat": chat})
+                data.append({"query": query, "articles": articles, "chat": chat})
                 print("Generated for case", i)
                 break
             else:
@@ -77,6 +85,12 @@ for i, c in enumerate(cases):
             continue
         except:
             raise
-with open("finetuning_chat_data.json", "w") as f:
-    f.writelines(json.dumps(data))
+    if len(data) == 10: 
+        with open(f"finetuning_chat_data_{data_count}.json", "w") as f:
+            f.writelines(json.dumps(data))
+        data_count+= 1
+        data = []
+if len(data) > 0:
+    with open(f"finetuning_chat_data_{data_count}.json", "w") as f:
+        f.writelines(json.dumps(data))
 print("Done chat...")
